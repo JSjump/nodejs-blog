9cf0d62fb1b1ff34b675e488197d677a258414b7
Zybtest <2013230787@qq.com>
2019-06-05 18:26:19 +0800

    '添加session中间件记录登录状态'

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var path = require('path');
-
+const session = require('express-session');
 var app = express();
+
+// 使用session中间件
+app.use(session({
+    secret:'keyboard cat', //对session id相关的cookie进行签名
+    resave:false, // 是否保存未初始化的会话
+    saveUninitialized:true
+}))
 
 var mongoose = require('mongoose');
 
@@ -21,7 +28,7 @@ var articleController = require('./controllers/article');
 app.set('views',path.join(__dirname,'views'));// 设置模板存放目录 若不设则默认views
 app.set('view engine','pug'); // 设置模板引擎为pug   pug特色block功能，extends继承。  区别于express的includes   
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public"))); // 设置静态目录
 // var props = {
 //     articles:[],
 //     title: 'Add article'
